25f640b832e54b760717082826f6261d1b6b6b22
Wahome Macharia <wahome@Wahomes-MacBook-Pro.local>
2023-05-04 15:19:30 +0300

    Add sql logging and fix n+1 query expansion

--- src/main/java/io/kwahome/graphqlinjava/repository/BookRepository.java
+++ src/main/java/io/kwahome/graphqlinjava/repository/BookRepository.java
@@ -2,8 +2,16 @@ package io.kwahome.graphqlinjava.repository;
 
 import io.kwahome.graphqlinjava.model.Author;
 import io.kwahome.graphqlinjava.model.Book;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
+    @NotNull
+    @Query("SELECT DISTINCT book FROM Book book JOIN FETCH book.author authors")
+    List<Book> findAll();
+
     Iterable<Book> findBooksByAuthor(Author author);
 }
